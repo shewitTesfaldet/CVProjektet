60d6d25c38d17d47c751b60c30d7322ab670d46c
S <121876460+shewitTesfaldet@users.noreply.github.com>
2023-12-18 13:00:36 +0100

    CV_Projekt och Projekt
    i

---
+++ CV/CV/Models/CV_projekt.cs
@@ -0,0 +1,9 @@
+﻿namespace CV.Models
+{
+    public class CV_projekt
+    {
+        public int CID { get; set; }
+
+        public int PID { get; set; }
+    }
+}
---
+++ CV/CV/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace CV.Models
+{
+   public class Project
+    {
+        public int PID { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
