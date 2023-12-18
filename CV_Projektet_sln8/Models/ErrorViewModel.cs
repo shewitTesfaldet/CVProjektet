4562bd30a40429ee2c1dbc9e8ea4a602b4ed087d
S <121876460+shewitTesfaldet@users.noreply.github.com>
2023-12-18 11:55:32 +0100

    NYNYNYNYNNY

--- CV_Projektet_sln8/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CV_Projektet_sln8.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
