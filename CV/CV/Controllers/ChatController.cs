de643dfbc4ca009993aca80126b1f76fd49c5171
S <121876460+shewitTesfaldet@users.noreply.github.com>
2023-12-26 14:36:52 +0100

    startsida klar

---
+++ CV/CV/Controllers/ChatController.cs
@@ -0,0 +1,38 @@
+﻿
+using CV.Models;
+using CV.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CV.Controllers
+{
+    // ChatController.cs
+
+        public class ChatController : Controller
+        {
+        private UserContext _userContext;
+
+        //kod för getlogged on user
+        private string getLogedOnUser;
+        public ChatController(UserContext context)
+        {
+            _userContext = context;
+        }
+            public IActionResult Index()
+            {
+
+            ViewBag.Meddelanden = _userContext.Chats.Where(x => x.UID.Equals(1))
+                        .ToList();
+            return View(new Chat());
+            }
+
+            [HttpPost]
+            public IActionResult SendMessage(Chat message)
+            {
+                _userContext.Chats.Add(message);
+                return RedirectToAction("Index");
+            }
+        }
+    }
+
+
--- CV/CV/Controllers/HomeController.cs
+++ CV/CV/Controllers/HomeController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Diagnostics;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Cryptography;
+using System.Net;
 
 namespace CV.Controllers
 {
@@ -25,15 +28,43 @@ namespace CV.Controllers
 
         public IActionResult Index()
         {
+            //Hämtar alla användare med utbildning i en lista
 
-            List<User> produktList = _userContext.Users.ToList();
-            Console.WriteLine($"Antal användare: {produktList.Count}");
-            ViewBag.Meddelande = "Antal i listan";
-            return View(produktList);
+            var data = (from user in _userContext.Users
+                        join cv in _userContext.CV_s on user.UID equals cv.UID into cvGroup
+                        from cv in cvGroup.DefaultIfEmpty()
+                        join cvEdu in _userContext.CV_Educations on cv.CID equals cvEdu.CID into cvEduGroup
+                        from cvEdu in cvEduGroup.DefaultIfEmpty()
+                        join edu in _userContext.Education on cvEdu.EdID equals edu.EdID into eduGroup
+                        from edu in eduGroup.DefaultIfEmpty()
+                        select new { user.UID, user.Firstname, user.Lastname, user.Username, user.Privat, Description = (edu == null ? null : edu.Description) }).ToList();
+            //sorterar listan utofrån de senaste inlaggda och de 5 första.
 
+
+            var result = data.GroupBy(x => x.UID)
+                             .Select(g => g.First())
+                             .OrderByDescending(x => x.UID)
+                             .Take(5)
+                             .ToList();
+
+            ViewBag.Result = result;
+            //Hämtar projekt utifrån användare och sorterar de senaste
+            var Presult = (from user in _userContext.Users
+                           join userProject in _userContext.UserProjects on user.UID equals userProject.UID
+                           join project in _userContext.Projects on userProject.PID equals project.PID
+                           orderby project.PID descending
+                           select new { user.Username, project.Title, project.Description })
+             .Take(5)
+             .ToList();
+
+            ViewBag.Presult = Presult;
+
+
+
+            return View();
 
         }
 
-
+        
     }
 }
