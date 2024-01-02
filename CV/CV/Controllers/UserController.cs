244bd08f13db630b9ca1d036eb98e5e25bd4cda1
kanelbullar123 <132896863+kanelbullar123@users.noreply.github.com>
2024-01-02 11:35:25 +0100

    Update UserController.cs

--- CV/CV/Controllers/UserController.cs
+++ CV/CV/Controllers/UserController.cs
@@ -42,6 +42,53 @@ namespace CV.Controllers
 
 
         }
+        [HttpGet]
 
+        public IActionResult Profile(int UID)
+        {
+            UID = 3;
+            User user = _userContext.Users.Find(UID);
+            ViewBag.Profile = user;
+            return View(user);
+        }
+
+        [HttpPost]
+        public IActionResult Profile(User updatedUser)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hitta användaren med det specifika UID (t.ex. 3) i databasen
+                var existingUser = _userContext.Users.Find(3);
+
+                if (existingUser == null)
+                {
+                    // Om användaren inte finns, hantera detta scenario, t.ex. visa felmeddelande.
+                    return NotFound();
+                }
+
+                // Uppdatera befintliga användarvärden med de nya värdena från updatedUser
+                existingUser.Password = updatedUser.Password;
+                existingUser.ConfirmPassword = updatedUser.ConfirmPassword;
+                existingUser.Firstname = updatedUser.Firstname;
+                existingUser.Lastname = updatedUser.Lastname;
+                existingUser.Epost = updatedUser.Epost;
+                existingUser.Adress = updatedUser.Adress;
+                existingUser.Privat = updatedUser.Privat;
+                existingUser.Username = updatedUser.Username;
+                // Spara ändringarna i databasen
+                _userContext.SaveChanges();
+
+                // Uppdatera ViewBag.Message med ett meddelande som bekräftar ändringarna
+                ViewBag.Message = "Profilen har uppdaterats";
+
+                // Återvänd till profilsidan
+                return View(existingUser);
+            }
+            else
+            {
+                // Om modellens tillstånd inte är giltigt, returnera vyn med fel
+                return View(updatedUser);
+            }
+        }
     }
 }
