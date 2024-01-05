224b0d17371738fe15496cf1a18a31c2dfbb2aa3
Farahduale <farah.isse003@gmail.com>
2024-01-05 23:04:31 +0100

    Nya projektet
    Nya projektsidan

---
+++ CV/Models/DeltagarListaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class DeltagarListaViewModel
+    {
+        public int UID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public bool Privat { get; set; }
+        public string ProjectDescription { get; set; }
+    }
+}
