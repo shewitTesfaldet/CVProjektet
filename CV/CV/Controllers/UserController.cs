909f90cd27f5aac53e4c54fff4dfe6f4ad5e80e1
kanelbullar123 <132896863+kanelbullar123@users.noreply.github.com>
2024-01-06 19:24:56 +0100

    Profil, update

--- CV/CV/Controllers/UserController.cs
+++ CV/CV/Controllers/UserController.cs
@@ -50,30 +50,6 @@ namespace CV.Controllers
         public IActionResult Profile()
         {
             User user = new User();
-/*            *//*            string Name = User.Identity.Name;
-*/
-        string Name = "user1";
-        if (Name != null)
-        {
-            user = _userContext.Users.FirstOrDefault(x => x.Username.Equals(Name));
-        }
-        if (user.Username == null)
-        {
-/*             Om användaren inte finns, returnera NotFound
-*/            return NotFound();
-        }
-
-
-        return View(user);
-    }
-
-
-
-        [Authorize]
-        [HttpGet]
-        public IActionResult Update()
-        {
-            User user = new User();
             /*            *//*            string Name = User.Identity.Name;
             */
             string Name = "user1";
@@ -92,102 +68,64 @@ namespace CV.Controllers
             return View(user);
         }
 
-        /*
-                [Authorize]
-                [HttpGet]
-                public IActionResult Profile()
-                {
-                    User user = new User();
-                    *//*            string Name = User.Identity.Name;
-                    */
-
-        /*HÅRDKODAD FÖR TEST*/
-//        string Name = "user1";
-//            if (Name != null)
-//            {
-//                user = _userContext.Users.FirstOrDefault(x => x.Username.Equals(Name));
-//            }
-//            if (user.Username == null)
-//            {
-//                // Om användaren inte finns, returnera NotFound
-//                return NotFound();
-//}
 
 
-//return View(user);
-//        }
-
-
-
-        [HttpPost]
-public IActionResult Update(User updatedUser)
-{
-    if (ModelState.IsValid)
-    {
-        string Name = "user1";
-
-        var userToUpdate = _userContext.Users.FirstOrDefault(x => x.Username.Equals(Name));
-
-        if (userToUpdate != null)
+        [Authorize]
+        [HttpGet]
+        public IActionResult Update()
         {
-            userToUpdate.Firstname = updatedUser.Firstname;
-            userToUpdate.Lastname = updatedUser.Lastname;
-            userToUpdate.Password = updatedUser.Password;
-            userToUpdate.Adress = updatedUser.Adress;
-            userToUpdate.Epost = updatedUser.Epost;
-
-            if (!string.IsNullOrEmpty(updatedUser.ConfirmPassword) && updatedUser.Password == updatedUser.ConfirmPassword)
+            User user = new User();
+            /*            *//*            string Name = User.Identity.Name;
+            */
+            string Name = "user1";
+            if (Name != null)
             {
-                updatedUser.Password = updatedUser.ConfirmPassword;
+                user = _userContext.Users.FirstOrDefault(x => x.Username.Equals(Name));
+            }
+            if (user.Username == null)
+            {
+
+                return NotFound();
             }
 
 
-            _userContext.SaveChanges();
+            return View(user);
+        }
 
 
-            return RedirectToAction("Profile");
+        [HttpPost]
+        public IActionResult Update(User updatedUser)
+        {
+            if (ModelState.IsValid)
+            {
+                string Name = "user1";
+
+                var userToUpdate = _userContext.Users.FirstOrDefault(x => x.Username.Equals(Name));
+
+                if (userToUpdate != null)
+                {
+                    userToUpdate.Firstname = updatedUser.Firstname;
+                    userToUpdate.Lastname = updatedUser.Lastname;
+                    userToUpdate.Password = updatedUser.Password;
+                    userToUpdate.Adress = updatedUser.Adress;
+                    userToUpdate.Epost = updatedUser.Epost;
+
+                    if (!string.IsNullOrEmpty(updatedUser.ConfirmPassword) && updatedUser.Password == updatedUser.ConfirmPassword)
+                    {
+                        updatedUser.Password = updatedUser.ConfirmPassword;
+                    }
+
+
+                    _userContext.SaveChanges();
+
+
+                    return RedirectToAction("Profile");
+                }
+            }
+
+            return View(updatedUser);
         }
     }
-
-    return View(updatedUser);
 }
 
 
-
-            /* {/*
-
-                 if (ModelState.IsValid)
-                 {
-                     User existingUser = _userContext.Users.FirstOrDefault(x=> x.Equals(loginUser));
-
-                     if (existingUser == null)
-                     {
-                         return NotFound();
-                     }
-                     existingUser.Password = updatedUser.Password;
-                     existingUser.ConfirmPassword = updatedUser.ConfirmPassword;
-                     existingUser.Firstname = updatedUser.Firstname;
-                     existingUser.Lastname = updatedUser.Lastname;
-                     existingUser.Epost = updatedUser.Epost;
-                     existingUser.Adress = updatedUser.Adress;
-                     existingUser.Privat = updatedUser.Privat;
-                     existingUser.Username = updatedUser.Username;
-                     // Spara ändringarna i databasen
-                     _userContext.SaveChanges();
-
-                     // Uppdatera ViewBag.Message med ett meddelande som bekräftar ändringarna
-                     ViewBag.Message = "Profilen har uppdaterats";
-
-                     // Återvänd till profilsidan
-                     return View(existingUser);
-                 }
-                 else
-                 {
-                     // Om modellens tillstånd inte är giltigt, returnera vyn med fel
-                     return View(updatedUser);
-                 }
-             }
-         }*//*
-             }*/
-    }
-    }
